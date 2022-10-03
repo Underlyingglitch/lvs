f71c45fab9985a9360ce593d3dac89dfebfe06b8
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-10-03 12:49:33 +0200

    Fixed DB problem

--- database/migrations/2022_09_27_164406_create_leerlingen_table.php
+++ database/migrations/2022_09_27_164406_create_leerlingen_table.php
@@ -15,13 +15,14 @@ return new class extends Migration
     {
         Schema::create('leerlingen', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
-            $table->unsignedBigInteger('buddie_id')->nullable();
-            $table->foreign('buddie_id')->references('id')->on('buddies');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('buddie_id')->unsigned()->nullable();
             $table->integer('leerlingnummer')->nullable();
             $table->string('klas')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('buddie_id')->references('id')->on('buddies')->nullOnDelete();
         });
     }
 
