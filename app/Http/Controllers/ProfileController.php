ba6de3027cd26bf5a3c716791cdf12d807932891
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2022-11-01 13:03:56 +0100

    Fixed problem where password was not validated

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,7 +30,7 @@ class ProfileController extends Controller
 
         $user = User::find(auth()->user()->id);
 
-        if (Hash::check($request->password, $user->password)) {
+        if (Hash::check($request->current_password, $user->password)) {
             $user->password = Hash::make($request->new_password);
             $user->save();
             return redirect('/');
